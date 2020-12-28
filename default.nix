35438e3cab687713b8e00a2ae1a6d4f6e6ead689
Ben Siraphob <bensiraphob@gmail.com>
2020-12-28 14:34:47 +0700

    Add system as an input

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 { sources ? import ./nix/sources.nix { }
 , pkgs ? import sources.nixpkgs { }
+, system ? builtins.currentSystem
 }:
 let
   # Nixpkgs extended with bootstrappable related packages
-  bootstrappable-pkgs = import ./pkgs { };
+  bootstrappable-pkgs = import ./pkgs { inherit system; };
 in
 bootstrappable-pkgs
