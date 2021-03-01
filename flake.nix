d9211c4b68603290ab7ea75a55f252c68e38c621
Ben Siraphob <bensiraphob@gmail.com>
2021-03-01 16:00:02 +0700

    Expose overlay

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
       in {
         inherit overlay;
-        inherit (pkgs) blynn-compiler;
+        packages = pkgs;
         defaultPackage = pkgs.blynn-compiler;
       });
 }
